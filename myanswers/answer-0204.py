b300e8227e75716e0250bba56cf4ad42f1c40ef5
PabloM26 <jpablo.marin2@udea.edu.co>
2026-05-14 17:15:02 -0500

    Update answer-0204.py

--- myanswers/answer-0204.py
+++ myanswers/answer-0204.py
@@ -3,25 +3,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.datasets import make_classification
 
-def generar_caso_de_uso_optimizar_bosque_aleatorio(**kwargs):
-
-    X, y = make_classification(n_samples=100, n_features=4, random_state=42)
-    
-
-    rf = RandomForestClassifier(random_state=42)
-    param_dist = {'n_estimators': [50, 100, 200], 'max_depth': [None, 10, 20]}
-    search = RandomizedSearchCV(rf, param_distributions=param_dist, n_iter=3, cv=3, random_state=42)
-    search.fit(X, y)
-    
-
-    caso_dict = {
-        "input": {"X": X, "y": y},
-        "output": {"mejores_parametros": search.best_params_}
-    }
-    
-
-    return caso_dict, "Caso de uso: Optimización de hiperparámetros con Random Search"
-
 def optimizar_bosque_aleatorio(X, y):
     # 1. Instanciar el modelo base
     rf = RandomForestClassifier(random_state=42)
